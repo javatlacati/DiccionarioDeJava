d58a7dc1fe3b15a57d99ebdd51a3658ea66691f0
Ruslan López <scherzo_16@hotmail.com>
2015-11-26 07:23:39 -0600

    wrap main activity

--- client-android/src/main/java/web/aprendiendola/dictionaryweb/aprendiendola/dictionary/DiccionarioDeJava/AndroidMain.java
+++ client-android/src/main/java/web/aprendiendola/dictionaryweb/aprendiendola/dictionary/DiccionarioDeJava/AndroidMain.java
@@ -1,9 +1,42 @@
 package web.aprendiendola.dictionaryweb.aprendiendola.dictionary.DiccionarioDeJava;
 
-public class AndroidMain {
-    private AndroidMain() {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AndroidMain extends Activity {
+
+    public AndroidMain() {
+//        try {
+//            Main.onPageLoad();
+//        } catch (Exception ex) {
+//            Logger.getLogger(AndroidMain.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
-    
+    //private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // obtain information you need
+        //
+        //SharedPreferences prefs = getApplicationContext().getSharedPreferences("karel.prefs", 0);
+        //new AndroidStorage(prefs);
+        //mInterstitialAd = new InterstitialAd(this,
+        //        "ca-app-pub-4207300711287771/3403503227");
+    //    AdView mAdView = new AdView(this, AdSize.BANNER, "");
+        //    AdRequest adRequest = new AdRequest();
+        //adRequest.Builder().build();
+        //   mAdView.loadAd(adRequest);
+         //mInterstitialAd.setAdUnitId("ca-app-pub-99999999999999999/9999999999");
+        // delegate to original activity
+        startActivity(new Intent(getApplicationContext(),
+                com.dukescript.presenters.Android.class));
+
+        finish();
+    }
+
     public static void main(String... args) throws Exception {
         Main.onPageLoad();
     }
