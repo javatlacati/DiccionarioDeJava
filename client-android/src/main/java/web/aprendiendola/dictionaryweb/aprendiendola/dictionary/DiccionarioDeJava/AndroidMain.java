ee524d8ffe3bdd40124bcc36cbb2a5dc9b3f1324
Ruslan López <scherzo_16@hotmail.com>
2015-12-04 03:02:22 -0600

    Added admob interstitial publicity

--- client-android/src/main/java/web/aprendiendola/dictionaryweb/aprendiendola/dictionary/DiccionarioDeJava/AndroidMain.java
+++ client-android/src/main/java/web/aprendiendola/dictionaryweb/aprendiendola/dictionary/DiccionarioDeJava/AndroidMain.java
@@ -3,8 +3,23 @@ package web.aprendiendola.dictionaryweb.aprendiendola.dictionary.DiccionarioDeJa
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class AndroidMain extends Activity {
+
+    private InterstitialAd mInterstitialAd;
+    private Handler mHandler;       // Handler to display the ad on the UI thread
+    private Runnable displayAd;     // Code to execute to perform this operation
 
     public AndroidMain() {
 //        try {
@@ -13,8 +28,11 @@ public class AndroidMain extends Activity {
 //            Logger.getLogger(AndroidMain.class.getName()).log(Level.SEVERE, null, ex);
 //        }
     }
-    //private InterstitialAd mInterstitialAd;
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +41,33 @@ public class AndroidMain extends Activity {
         //
         //SharedPreferences prefs = getApplicationContext().getSharedPreferences("karel.prefs", 0);
         //new AndroidStorage(prefs);
-        //mInterstitialAd = new InterstitialAd(this,
-        //        "ca-app-pub-4207300711287771/3403503227");
-    //    AdView mAdView = new AdView(this, AdSize.BANNER, "");
-        //    AdRequest adRequest = new AdRequest();
-        //adRequest.Builder().build();
-        //   mAdView.loadAd(adRequest);
-         //mInterstitialAd.setAdUnitId("ca-app-pub-99999999999999999/9999999999");
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-4207300711287771/3403503227");
+        AdView adView = new AdView(getBaseContext());//(AdView) this.findViewById(R.id.adView);
+        adView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-4207300711287771/3403503227");
+        //adView.setId(50);
+        AdRequest adRequest = new AdRequest.Builder()
+                // Add a test device to show Test Ads
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("jhkjh")
+                .build();
+
+        // Load ads into Banner Ads
+        adView.loadAd(adRequest);
+
+        // Load ads into Interstitial Ads
+        mInterstitialAd.loadAd(adRequest);
+
+        // Prepare an Interstitial Ad Listener
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
         // delegate to original activity
         startActivity(new Intent(getApplicationContext(),
                 com.dukescript.presenters.Android.class));
@@ -37,7 +75,23 @@ public class AndroidMain extends Activity {
         finish();
     }
 
-    public static void main(String... args) throws Exception {
+    /**
+     *
+     */
+    public void displayInterstitial() {
+        // If Ads are loaded, show Interstitial else show nothing.
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
         Main.onPageLoad();
     }
+
 }
